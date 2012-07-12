b932484474008e482d6719d33ddcedaae214b33a
Matt Ryan <mryan@sankynet.com>
2012-07-12 09:15:22 -0400

    Add drush

---
+++ drush/drush.symlink/ch.alias.drushrc.php
@@ -0,0 +1,10 @@
+<?php
+
+$aliases['ch'] = array (
+  'root' => '/Users/matt/Sites/ch.dev/sites/ch2012/ch2012.sankyserver.com',
+  'uri' => 'ch2012.dev',
+  'remote-host' => 'localhost',
+  'remote-user' => 'root',
+);
+
+?>
--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-export PATH="/Applications/MAMP/Library/bin:/Applications/MAMP/bin/php5.3/bin:/Applications/SenchaSDKTools-2.0.0-beta3:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/usr/local/git/bin:$DOTS/bin:$PATH"
+export PATH="/Applications/MAMP/Library/bin:/Applications/MAMP/bin/php5.3/bin:/Applications/SenchaSDKTools-2.0.0-beta3:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/usr/local/git/bin:$DOTS/bin"
