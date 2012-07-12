459ee8326715136b76b0c3b340e76f4ab3d8875d
Matt Ryan <mryan@sankynet.com>
2012-07-12 09:32:49 -0400

    Custom zsh functions
    c => cd into sites folder
    smartextract => wtf do you think it does?

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,3 +1,6 @@
+fpath=($DOTS/zsh/functions $fpath)
+
+autoload -U $DOTS/zsh/functions/*(:t)
 bindkey -v
 bindkey '^N' history-beginning-search-forward
 bindkey '^P' history-beginning-search-backward
