2ac4d6cee490750756ccde86d86e59d324622ec6
Matt Ryan <mryan@sankynet.com>
2012-11-02 12:13:22 -0400

    House cleaning
    + Remove todo (never use)
    + Move all completion scripts to .zsh type
    + Source all completion scripts with new type

--- git/completion.zsh
+++ git/completion.zsh
@@ -1,7 +1,5 @@
-# Uses git's autocompletion for inner commands. Assumes an install of git's
-# bash `git-completion` script at $completion below (this is where Homebrew
-# tosses it, at least).
-completion=/usr/local/etc/bash_completion.d/git-completion.bash
+# Uses git's autocompletion for inner commands.
+completion=~/.git-completion.bash
 
 if test -f $completion
 then
