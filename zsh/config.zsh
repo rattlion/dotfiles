35fcfc2a71f248dbc4686634765d666f9157db1a
Matt Ryan <mryan@sankynet.com>
2013-01-24 08:46:10 -0500

    Colored Man Pages

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -6,3 +6,15 @@ bindkey '^N' history-beginning-search-forward
 bindkey '^P' history-beginning-search-backward
 bindkey '^[[B' history-beginning-search-forward
 bindkey '^[[A' history-beginning-search-backward
+
+man() {
+  env \
+    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    man "$@"
+}
