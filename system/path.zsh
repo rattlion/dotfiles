421fecb7b974ac60c2fd9c26b1f4280b856b8a57
Matt Ryan <mryan@sankynet.com>
2013-04-12 13:35:50 -0400

    PATH COLUSION
    + And global gitignore

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-export PATH="$DOTS/bin:$HOME/.rvm/bin:/usr/local/heroku/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:/usr/X11/bin"
+export PATH=$PATH:$DOTS/bin:/usr/local/heroku/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:/usr/X11/bin
