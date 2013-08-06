854b06f3254fe0084d2b8e3617d3fab7eb757515
Matt Ryan <m@mrevd.me>
2013-08-06 10:30:09 -0400

    Remove hub

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,11 +1,3 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if [[ -f $hub_path ]]
-then
-  alias git=$hub_path
-fi
-
 # The rest of my fun git aliases
 alias gf='git fetch --all -p'
 alias gm='git merge --no-ff'
