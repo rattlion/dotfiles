315574028742d54f287ec633e4bff7ed9b5cba3d
Matt Ryan <m@mrevd.me>
2013-12-14 12:31:20 -0500

    New comepletion style, god damn rvm path, and shift-tab compl

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -38,3 +38,4 @@ bindkey '^N' history-beginning-search-forward
 bindkey '^P' history-beginning-search-backward
 bindkey '^[[B' history-beginning-search-forward
 bindkey '^[[A' history-beginning-search-backward
+bindkey '^[[Z' reverse-menu-complete
