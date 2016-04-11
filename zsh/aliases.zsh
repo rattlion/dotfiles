3bd475af80453fac4b66c8e2da0d1011af55be25
Matt Ryan <m@mrevd.me>
2016-04-10 19:19:28 -0700

    ll

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,7 @@ alias sudo="sudo "
 alias dots="cd $ZSH"
 alias ls='ls --color=auto'
 alias l="ls -lAh"
+alias ll="ls -la"
 alias grep='grep --color=auto'
 alias reload!='. ~/.zshrc'
 alias selfupdate="brew update && brew upgrade --all && brew cleanup && rvm get stable"
