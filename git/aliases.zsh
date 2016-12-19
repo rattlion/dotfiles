f59711e242f5b878fd63245afc7452b1be5e45cb
Matt Ryan <m@mrevd.me>
2016-12-18 21:00:28 -0800

    lola shorthand

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,6 +1,5 @@
 # Aliases
 alias g='git'
-alias gi="git init"
 alias ga='git add -A :/'
 alias gb='git branch'
 alias gba='git branch -a'
@@ -11,11 +10,14 @@ alias gca='git commit -v -a'
 alias gco='git checkout'
 alias gd='git diff'
 alias gf='git fetch --all -p'
+alias gi="git init"
 alias gl='git pull --prune'
+alias glol='git lol'
+alias glola='git lola'
 alias glr='git pull --rebase'
 alias gm='git merge --no-ff'
-alias gp='git push origin HEAD'
 alias gp!='git push origin HEAD -f'
+alias gp='git push origin HEAD'
 alias gr='git reset'
 alias grh='git reset --hard'
 alias grhh='git reset --hard HEAD'
