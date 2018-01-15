2d150d493ae4b35c432913f6f25fca3a60050c9f
Matt Ryan <>
2018-01-15 14:41:07 -0800

    move mac speicific things into secrets... anywhere but here

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,7 @@
 alias sudo="sudo "
 alias dots="cd $ZSH"
-alias ls='ls --color=auto'
+alias ls="ls --color=auto"
 alias l="ls -lAh"
 alias ll="ls -la"
-alias grep='grep --color=auto'
-alias reload!='. ~/.zshrc'
-alias selfupdate="brew update && brew upgrade && brew cleanup"
-alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/"
+alias grep="grep --color=auto"
+alias reload!=". ~/.zshrc"
