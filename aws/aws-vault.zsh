f212330a30a76054d8b23f34079ac855f1bcfec2
Matt Ryan <>
2020-03-14 16:29:57 -0700

    AWS Vault, Linux Brew, and 1pwcli

---
+++ aws/aws-vault.zsh
@@ -0,0 +1,10 @@
+export AWS_VAULT_FILE_PASSPHRASE="3mN6+PuB7htXIWLMfzvX0kMcsK4bdfTjtrS+pEVZpjk=" # Avoid entering a passphrase when using aws-vault
+export AWS_VAULT_BACKEND="file" # Use the file backend of aws-vault
+export AWS_SESSION_TTL="12h" # Set the session ttl to 12 hours to avoid reentering MFA multiple times a day
+export AWS_ASSUME_ROLE_TTL="12h"
+
+alias sgorder_mfa='op get totp "SG AWS"'
+
+aws-sg-order () {
+  aws-vault exec --mfa-token $(sgorder_mfa) sweetgreen-order $@
+}
