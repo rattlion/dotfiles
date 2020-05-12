af1ca3d820722fb08d9b58b93e58cf9b06d1785a
Matt Ryan <matt.ryan@sweetgreen.com>
2020-05-12 09:51:09 -0700

    Remove linuxbrew, rework aws-vault SG functions

--- aws/aws-vault.zsh
+++ aws/aws-vault.zsh
@@ -3,8 +3,12 @@ export AWS_VAULT_BACKEND="file" # Use the file backend of aws-vault
 export AWS_SESSION_TTL="12h" # Set the session ttl to 12 hours to avoid reentering MFA multiple times a day
 export AWS_ASSUME_ROLE_TTL="12h"
 
-alias sgorder_mfa='op get totp "SG AWS"'
+alias sg_mfa='op get totp "SG AWS"'
+
+aws-sg-root () {
+  aws-vault exec --mfa-token $(sg_mfa) default $@
+}
 
 aws-sg-order () {
-  aws-vault exec --mfa-token $(sgorder_mfa) sweetgreen-order $@
+  aws-vault exec --mfa-token $(sg_mfa) sweetgreen-order $@
 }
