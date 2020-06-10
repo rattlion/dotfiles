bf92929ccd285af04da09f32a59e44b210f0200c
Matt Ryan <matt.ryan@sweetgreen.com>
2020-06-10 09:30:53 -0700

    AWS-VAULT alias

--- aws/aws-vault.zsh
+++ aws/aws-vault.zsh
@@ -3,6 +3,7 @@ export AWS_VAULT_BACKEND="file" # Use the file backend of aws-vault
 export AWS_SESSION_TTL="12h" # Set the session ttl to 12 hours to avoid reentering MFA multiple times a day
 export AWS_ASSUME_ROLE_TTL="12h"
 
+alias av='aws-vault'
 alias sg_mfa='op get totp "SG AWS"'
 
 aws-sg-root () {
