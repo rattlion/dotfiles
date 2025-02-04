c93fb9b78d2c479cc1f199fad4b2d7585e527a10
Matt Ryan <mcjryan@amazon.com>
2025-02-03 16:49:48 -0800

    nvim dashboard

--- vim/.config/nvim.symlink/lua/plugins/dashboard.lua
+++ vim/.config/nvim.symlink/lua/plugins/dashboard.lua
@@ -10,6 +10,20 @@ return {
         },
         shortcut = {
           { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+          {
+            icon = ' ',
+            icon_hl = '@variable',
+            desc = 'Files',
+            group = 'Label',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+          {
+            desc = '🌲 Nvim Tree',
+            group = 'DiagnosticHint',
+            action = 'NvimTreeOpen',
+            key = 't',
+          },
         },
       },
     }
