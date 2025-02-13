5efd7e2c5990db24da03f0a9db0d3b7d977f87ea
Matt Ryan <mcjryan@amazon.com>
2025-02-12 16:09:27 -0800

    ASDF completion

--- asdf/completion.zsh
+++ asdf/completion.zsh
@@ -1,5 +1,6 @@
+# necessary completion install steps
 # $ mkdir -p "${ASDF_DATA_DIR:-$HOME/.asdf}/completions"
 # $ asdf completion zsh > "${ASDF_DATA_DIR:-$HOME/.asdf}/completions/_asdf"
 
 # append completions to fpath
-fpath=(${ASDF_DIR}/completions $fpath)
+fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
