b83163597d023ecf1155934309d3692bf91b1a74
Matt Ryan <mcjryan@amazon.com>
2025-08-07 15:04:35 -0700

    Q cli initial organization

---
+++ amazon/q-postload.zsh
@@ -0,0 +1,2 @@
+# Amazon Q post block. Keep at the bottom of this file.
+[[ -f "${HOME}/Library/Application Support/amazon-q/shell/zshrc.post.zsh" ]] && builtin source "${HOME}/Library/Application Support/amazon-q/shell/zshrc.post.zsh"
---
+++ amazon/q-preload.zsh
@@ -0,0 +1,2 @@
+# Amazon Q pre block. Keep at the top of this file.
+[[ -f "${HOME}/Library/Application Support/amazon-q/shell/zshrc.pre.zsh" ]] && builtin source "${HOME}/Library/Application Support/amazon-q/shell/zshrc.pre.zsh"
