f6c0c94373b89b3ef6a5acd07a286d7d292418ee
Matt Ryan <mcjryan@amazon.com>
2025-10-29 15:29:26 -0700

    asdf -> mise

--- asdf/fpath.zsh
+++
@@ -1,6 +0,0 @@
-# necessary completion install steps
-# $ mkdir -p "${ASDF_DATA_DIR:-$HOME/.asdf}/completions"
-# $ asdf completion zsh > "${ASDF_DATA_DIR:-$HOME/.asdf}/completions/_asdf"
-
-# append completions to fpath
-fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
---
+++ mise/mise.zsh
@@ -0,0 +1,5 @@
+# auto complete instructions
+# look where zsh search for completions with
+# echo $fpath | tr ' ' '\n'
+# mise completion zsh  > /usr/local/share/zsh/site-functions/_mise
+eval "$(mise activate zsh)"
