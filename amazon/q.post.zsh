67c58b547cd47ec0fc3cc5d0969dc7b6d1511fc7
Matt Ryan <mcjryan@amazon.com>
2025-12-11 07:29:15 -0800

    Q -> Kiro

--- amazon/q.post.zsh
+++
@@ -1,2 +0,0 @@
-# Amazon Q post block. Keep at the bottom of this file.
-[[ -f "${HOME}/Library/Application Support/amazon-q/shell/zshrc.post.zsh" ]] && builtin source "${HOME}/Library/Application Support/amazon-q/shell/zshrc.post.zsh"
--- amazon/q.pre.zsh
+++
@@ -1,2 +0,0 @@
-# Amazon Q pre block. Keep at the top of this file.
-[[ -f "${HOME}/Library/Application Support/amazon-q/shell/zshrc.pre.zsh" ]] && builtin source "${HOME}/Library/Application Support/amazon-q/shell/zshrc.pre.zsh"
