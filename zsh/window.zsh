e82dad434b753e8dcabcfd29064adf3f95fb0326
Matt Ryan <matt.cj.ryan@gmail.com>
2026-04-18 12:50:08 -0700

    Update title

--- zsh/window.zsh
+++ zsh/window.zsh
@@ -1,18 +1,23 @@
-# From http://dotfiles.org/~_why/.zshrc
 # Sets the window title nicely no matter where you are
 function title() {
   # escape '%' chars in $1, make nonprintables visible
-  a=${(V)1//\%/\%\%}
+  local a=${(V)1//\%/\%\%}
 
   # Truncate command, and join lines.
   a=$(print -Pn "%40>...>$a" | tr -d "\n")
+  
+  print -Pn "\e]2;$a - $2\a"
 
-  case $TERM in
-  screen)
-    print -Pn "\ek$a:$3\e\\" # screen title (in ^A")
-    ;;
-  xterm*|rxvt)
-    print -Pn "\e]2;$2\a" # plain xterm title ($3 for pwd)
-    ;;
-  esac
+  # Set the tmux window name (shorter version for the tmux status bar)
+  if [[ -n "$TMUX" ]]; then
+    print -Pn "\ek$a\e\\"
+  fi
+}
+
+precmd() {
+  title "zsh" "%m(%~)" "%~"
+}
+
+preexec() {
+  title "$1" "%m(%~)" "%~"
 }
