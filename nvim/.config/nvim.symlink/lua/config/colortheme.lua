d3698e621494dcf5b15273f6c4a533a67b905b9f
Matt Ryan <mattryan@netflix.com>
2026-05-15 15:05:07 -0700

    Intelligent light and dark mode

--- nvim/.config/nvim.symlink/lua/config/colortheme.lua
+++ nvim/.config/nvim.symlink/lua/config/colortheme.lua
@@ -1,3 +1,16 @@
-vim.cmd.colorscheme("catppuccin")
-vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000" })
+local function apply_theme()
+  if vim.o.background == "dark" then
+    vim.cmd.colorscheme("catppuccin")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000" })
+  else
+    vim.cmd.colorscheme("catppuccin")
+  end
+end
+
+apply_theme()
+
+vim.api.nvim_create_autocmd("OptionSet", {
+  pattern = "background",
+  callback = apply_theme,
+})
